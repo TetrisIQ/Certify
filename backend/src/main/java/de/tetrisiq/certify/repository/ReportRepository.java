e7d0e4ff39d69bd7606acaf34efbb5e78ab5d213
alex <tetrisiq@tuta.io>
2021-03-21 14:05:38 +0100

    remvove frontend / backend folder move app folder to root

--- backend/src/main/java/de/tetrisiq/certify/repository/ReportRepository.java
+++
@@ -1,7 +0,0 @@
-package de.tetrisiq.certify.repository;
-
-import de.tetrisiq.certify.model.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, String> {
-}
