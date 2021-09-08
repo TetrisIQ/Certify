37b8d8c8123a9059f4cb2efbafc768f1bed9a8d9
alwerner <alexander.werner@bonprix.net>
2021-09-08 14:53:51 +0200

    feat: Delete all files

--- src/main/java/de/tetrisiq/certify/app/config/WebSecurityInterface.java
+++
@@ -1,7 +0,0 @@
-package de.tetrisiq.certify.app.config;
-
-import org.springframework.security.crypto.password.PasswordEncoder;
-
-public interface WebSecurityInterface {
-    PasswordEncoder passwordEncoder();
-}
--- src/main/java/de/tetrisiq/certify/app/repository/ReportRepository.java
+++
@@ -1,8 +0,0 @@
-package de.tetrisiq.certify.app.repository;
-
-import de.tetrisiq.certify.app.model.Report;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReportRepository extends JpaRepository<Report, String> {
-
-}
